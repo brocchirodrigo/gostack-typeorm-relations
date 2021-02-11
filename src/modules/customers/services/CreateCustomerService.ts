9c1aa259f88d0b3068998e7dfdc206d94d8168a8
brocchirodrigo <rodrigo.brocchi@gmail.com>
2021-02-10 23:04:25 -0300

    Conclusão

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customerEmailExists = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (customerEmailExists) {
+      throw new AppError('This user already exists');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
